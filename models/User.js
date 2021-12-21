0674a821ebb4f9f096093058c42a18c28342429f
Monika Kothari <mkothari.1993@gmail.com>
2021-12-20 21:08:44 -0600

    added User model

--- models/User.js
+++ models/User.js
@@ -3,3 +3,46 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class User extends Model {}
+
+User.init(
+  {
+    id: {
+      //Defines data type as a Integer
+      type: DataTypes.INTEGER,
+      //Defines that it shouldn't allow null values
+      allowNull: false,
+      //Defines that this is the Primary Key
+      primaryKey: true,
+      //Defines to auto increment
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;

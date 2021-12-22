b9be5d8025b6667fcae441f88f5bbf3941474908
leticianardi <leticia.tnardi@gmail.com>
2021-12-22 14:18:16 -0800

    update category and tag in models/index.js

--- models/index.js
+++ models/index.js
@@ -23,16 +23,26 @@ Post.belongsTo(Category, {
   foreignKey: "category_id",
 });
 
-//Category has many posts
-Category.hasMany(Post, {
-  foreignKey: "category_id",
-});
-
 //Post belongs to many tags (through PostTag)
 Post.belongsToMany(Tag, {
   through: PostTag,
   foreignKey: "post_id",
 });
+
+//Category has many posts
+Category.hasMany(Post, {
+  foreignKey: "category_id",
+});
+
+Category.hasMany(Tag, {
+  foreignKey: "category_id",
+});
+
+
+Tag.belongsToMany(Category, {
+  through: "PostTag",
+  foreignKey: "tag_id"
+})
 
 // Tags belongToMany Post (through PostTag)
 Tag.belongsToMany(Post, {
@@ -40,56 +50,6 @@ Tag.belongsToMany(Post, {
   foreignKey: "tag_id",
 });
 
-// User.belongsToMany(Post, {
-//   through: Tag,
-//   as: "voted_posts",
 
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Post.belongsToMany(User, {
-//   through: Vote,
-//   as: "voted_posts",
-//   foreignKey: "post_id",
-//   onDelete: "SET NULL",
-// });
-
-// Vote.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Vote.belongsTo(Post, {
-//   foreignKey: "post_id",
-//   onDelete: "SET NULL",
-// });
-
-// User.hasMany(Vote, {
-//   foreignKey: "user_id",
-// });
-
-// Post.hasMany(Vote, {
-//   foreignKey: "post_id",
-// });
-
-// Comment.belongsTo(User, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Comment.belongsTo(Post, {
-//   foreignKey: "post_id",
-//   onDelete: "SET NULL",
-// });
-
-// User.hasMany(Comment, {
-//   foreignKey: "user_id",
-//   onDelete: "SET NULL",
-// });
-
-// Post.hasMany(Comment, {
-//   foreignKey: "post_id",
-// });
 
 module.exports = { User, Post, Category, Tag, PostTag };

06a5ee348c135dc951e959bf1adffa5d0a60a41b
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2021-12-22 18:10:17 -0800

    login,logout,server update

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 //Imports routes
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 //Imports sequelize connection
 const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
 
 //Syncs sequelize models to the database, then turn on the server
 sequelize.sync({force:false}).then(() => {

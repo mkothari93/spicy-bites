1dbc5f7a3941b802dcde30fb2c108f2b9d787cc4
leticianardi <leticia.tnardi@gmail.com>
2021-12-28 11:11:35 -0800

    moved const sess to top of server.js

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -30,14 +41,5 @@ sequelize.sync({force:false}).then(() => {
     console.log(`App listening on port ${PORT}!`);
   });
 })
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: true,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
 
 app.use(session(sess));

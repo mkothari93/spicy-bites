4641f68ba0065c4f4daa676cb395ba99859eabfc
leticianardi <leticia.tnardi@gmail.com>
2022-01-03 21:14:58 -0800

    have no idea

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,30 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/logout', (req, res) => {
+  res.render('logout');
+});
+
+router.get('/homepage', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'recipe_name', 'recipe_body', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // get all posts
 router.get('/', (req, res) => {
   Post.findAll({
@@ -48,7 +72,7 @@ router.get('/post/:id', (req, res) => {
       }
       // serialize the data
       const post = dbPostData.get({ plain: true });
-
+      console.log('post', post);
       // pass data to template
       res.render('single-post', { post });
     })
